06abe79455b96c8d0e8b6d9270cc1895e52d673b
jiazengp <jiazengp@gmail.com>
2021-10-04 09:51:28 +0800

    feat: add anchor

--- docs/.vuepress/layout/layout.vue
+++ docs/.vuepress/layout/layout.vue
@@ -1,5 +1,8 @@
 <template>
   <Layout>
+    <template #page-top>
+      <Anchor v-show="anchorRight" />
+    </template>
     <template #page-bottom>
       <footer class="docs-footer page-meta">
         <div :class="$style.footerContainer" role="presentation">
@@ -13,7 +16,12 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { usePageFrontmatter } from '@vuepress/client'
 import Layout from '@vuepress/theme-default/lib/client/layouts/Layout.vue'
+
+const frontmatter = usePageFrontmatter()
+const anchorRight = ref(frontmatter.value.anchorRight ?? true)
 </script>
 
 <style module lang="scss">
